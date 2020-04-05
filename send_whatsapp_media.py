0140a2d67a1443ab66effe8b23fc32a8ab734386
Sri Hari S <cb.en.u4cse17457@cb.students.amrita.edu>
2020-04-05 09:30:12 +0530

    Add files via upload

---
+++ send_whatsapp_media.py
@@ -0,0 +1,19 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = 'AC4c89aeb25f388bd60d6f4dcabae7bc18'
+auth_token = '74fdf75126208443c6136ac000849e94'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+    .create(
+         media_url=['https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'],
+         from_='whatsapp:+14155238886',
+         body="it's taco time!!",
+         to='whatsapp:+919550643340'
+     )
+
+print(message.sid)
